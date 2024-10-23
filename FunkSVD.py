9b836999311d1c26f96985a80127394581474b2d
Igor Rahzel <igorrahzel@gmail.com>
2024-10-23 13:55:23 -0300

    conseguindo treinar o modelo

--- FunkSVD.py
+++ FunkSVD.py
@@ -4,12 +4,12 @@ import pandas as pd
 class FunkSVD:
     def __init__(self,dataframe):
         self.dataframe = dataframe
-        self.m = dataframe['userId'].nunique() 
-        self.n = dataframe['itemId'].nunique()
+        self.m = dataframe['UserId'].nunique() 
+        self.n = dataframe['ItemId'].nunique()
 
         # Create a mapping for userId and itemId
-        self.user_to_index = {user: idx for idx, user in enumerate(dataframe['userId'].unique())}
-        self.item_to_index = {item: idx for idx, item in enumerate(dataframe['itemId'].unique())}
+        self.user_to_index = {user: idx for idx, user in enumerate(dataframe['UserId'].unique())}
+        self.item_to_index = {item: idx for idx, item in enumerate(dataframe['ItemId'].unique())}
     
     def _initializePQ(self,k):
         self.P = 5/np.sqrt(5) * np.random.randn(self.m,k)
@@ -28,21 +28,48 @@ class FunkSVD:
         #Iterating over the epochs
         for epoch in range(epochs):
             total_loss = 0
+            i = 0
             #Iterating over the batches:
             for batch in self._getMiniBatch(batch_size):
-                users_idx = batch['userId'].map(self.user_to_index)
-                items_idx = batch['itemId'].map(self.item_to_index)
+                users_idx = batch['UserId'].map(self.user_to_index).to_numpy()
+                items_idx = batch['ItemId'].map(self.item_to_index).to_numpy()
 
                 PredictedRatings = self.P[users_idx,:] @ self.Q[:,items_idx]
                 
-                error = batch['Rating'] - PredictedRatings
+                #New mapping to acces predictions in the PredictedRatings matrix
+                batch_user_to_index = {user: idx for idx, user in enumerate(batch['UserId'].unique())}
+                batch_item_to_index = {item: idx for idx, item in enumerate(batch['ItemId'].unique())}
+                #batch_users_idx = batch['UserId'].map(batch_user_to_index)
+                #batch_items_idx = batch['ItemId'].map(batch_item_to_index)
+                predictions = []
+                for index,row in batch.iterrows():
+                    user = row['UserId']
+                    item = row['ItemId']
 
-                P_new = self.P[users_idx,:] + lr*(error * self.Q[:,items_idx] - lamda * self.P[users_idx,:])
-                Q_new = self.Q[:,items_idx] + lr*(error * self.P[users_idx,:] - lamda * self.Q[:,items_idx])
+                    batch_user_idx = batch_user_to_index[user]
+                    batch_item_idx = batch_item_to_index[item]
+
+                    prediction = PredictedRatings[batch_user_idx,batch_item_idx]
+                    predictions.append(prediction)
+
+                predictions = np.array(predictions)
+                
+                error = batch['Rating'].to_numpy() - predictions
+
+                # Reshape error for broadcasting
+                error = error[:, np.newaxis]  # Shape (batch_size, 1)
+
+                P_new = self.P[users_idx,:] + lr*((error * self.Q[:,items_idx].T) - (lamda * self.P[users_idx,:]))
+                Q_new = self.Q[:,items_idx].T + lr*(error * self.P[users_idx,:] - lamda * self.Q[:,items_idx].T)
 
                 self.P[users_idx,:] = P_new
-                self.Q[:,items_idx] = Q_new
+                self.Q[:,items_idx] = Q_new.T
 
+                n = len(self.dataframe)
+                iterations = n/batch_size
+
+                print(f"iteration {i+1}  of {iterations}")
+                i = i+1
                 # Calculate total loss (MSE)
                 total_loss += np.sum(error**2)
 
@@ -50,5 +77,10 @@ class FunkSVD:
             avg_loss = total_loss / len(self.dataframe)
             print(f"Epoch {epoch+1}/{epochs}, Loss: {avg_loss}")
 
-
-
+    def train(self,k=100,batch_size = 10,lr = 0.01,lamda = 0.02, epochs = 30):
+        self._MiniBatchGradientDescent(k,batch_size,lr,lamda , epochs)
+    
+    def prediction(self,userId,itemId):
+        user_idx = self.user_to_index[userId]
+        item_idx = self.item_to_index[itemId]
+        return self.P[user_idx,:] @ self.Q[:,item_idx]
--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import pandas as pd
 import numpy as np
-from MatrixBuilder import MatrixBuilder
+from TrainTestSplit import TrainTestSplit
+from FunkSVD import FunkSVD
 
 
 
 print(pd.__version__)
 print(np.__version__)
 
-#Building User-Item matrix for train and test sets
-builder = MatrixBuilder()
-user_item_matrix = builder()
-user_item_matrix = user_item_matrix.fillna(0)
-print(user_item_matrix)
+splitter = TrainTestSplit()
+train_data, test_data = splitter()
+
+model = FunkSVD(train_data)
+model.train()
+
+#TEST PREDICTIONS ON TRAINING SET
+#TEST PREDICTION ON TEST SET
